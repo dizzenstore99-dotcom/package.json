986008f656eba2eb453d955d05cf1a7d5e1b4779
lexzymods-executed <dizzenstore99@gmail.com>
2026-07-16 02:06:25 +0700

    Remove package.json content from index.js

--- index.js
+++ index.js
@@ -1,21 +1,11 @@
-{
-  "name": "baileys-newsletter-bot",
-  "version": "1.0.0",
-  "description": "WhatsApp Bot",
-  "main": "index.js",
-  "type": "commonjs",
-  "scripts": {
-    "start": "node index.js",
-    "dev": "node --watch index.js"
-  },
-  "engines": {
-    "node": ">=20"
-  },
-  "dependencies": {
-    "@whiskeysockets/baileys": "github:LexzyModsV2/Baileys",
-    "axios": "^1.11.0",
-    "chalk": "^5.6.2",
-    "pino": "^9.8.0",
-    "qrcode-terminal": "^0.12.0"
-  }
-}
+package.json
+        │
+npm install
+        │
+index.js  ← kode utama bot
+        │
+node index.js
+        │
+Login QR / Pairing
+        │
+Bot Online
