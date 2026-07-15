beeae58892cc1e4ee0557724ad1ffcef5cc9a71b
lexzymods-executed <dizzenstore99@gmail.com>
2026-07-16 02:01:55 +0700

    Initialize package.json for WhatsApp Bot project

---
+++ index.js
@@ -0,0 +1,21 @@
+{
+  "name": "baileys-newsletter-bot",
+  "version": "1.0.0",
+  "description": "WhatsApp Bot",
+  "main": "index.js",
+  "type": "commonjs",
+  "scripts": {
+    "start": "node index.js",
+    "dev": "node --watch index.js"
+  },
+  "engines": {
+    "node": ">=20"
+  },
+  "dependencies": {
+    "@whiskeysockets/baileys": "github:LexzyModsV2/Baileys",
+    "axios": "^1.11.0",
+    "chalk": "^5.6.2",
+    "pino": "^9.8.0",
+    "qrcode-terminal": "^0.12.0"
+  }
+}
